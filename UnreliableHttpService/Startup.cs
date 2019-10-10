c0dea70603628ba051f3d31d6769662ca31e3300
Joe Gardner <jgardner@clearent.com>
2019-10-09 21:08:31 -0400

    Upgrade to .Net Core 3.0

--- UnreliableHttpService/Startup.cs
+++ UnreliableHttpService/Startup.cs
@@ -24,18 +24,17 @@ namespace UnreliableHttpService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
+                endpoints.MapControllers();
+            });
         }
     }
 }
